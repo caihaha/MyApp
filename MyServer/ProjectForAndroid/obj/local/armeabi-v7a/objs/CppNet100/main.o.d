85f1ae84e87c7163a24705c2b1f0bd1ff21dcecc
caihaha <1030878894@qq.com>
2021-05-27 11:44:45 +0800

    版本代码提交

---
+++ MyServer/ProjectForAndroid/obj/local/armeabi-v7a/objs/CppNet100/main.o.d
@@ -0,0 +1,91 @@
+./obj/local/armeabi-v7a/objs/CppNet100/main.o: jni/main.cpp \
+  jni/../../depends/include\EasyEpollServer.hpp \
+  jni/../../depends/include/EasyTcpServer.hpp \
+  jni/../../depends/include/CELL.hpp \
+  jni/../../depends/include/MessageHeader.hpp \
+  jni/../../depends/include/CELLTimestamp.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\chrono \
+  jni/../../depends/include/CELLTask.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\thread \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\mutex \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\list \
+  jni/../../depends/include/CELLThread.hpp \
+  jni/../../depends/include/CELLSemaphore.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\condition_variable \
+  jni/../../depends/include/CELLLog.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\ctime \
+  jni/../../depends/include/CELLClient.hpp \
+  jni/../../depends/include/CELLBuffer.hpp \
+  jni/../../depends/include/CELLNetWork.hpp \
+  jni/../../depends/include/CELLServer.hpp \
+  jni/../../depends/include/INetEvent.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map \
+  jni/../../depends/include/CELLConfig.hpp \
+  E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\atomic \
+  jni/../../depends/include/CELLEpollServer.hpp \
+  jni/../../depends/include/CELLEpoll.hpp \
+  jni/../../depends/include\EasyEpollClient.hpp \
+  jni/../../depends/include/EasyTcpClient.hpp \
+  jni/../../depends/include\CELLMsgStream.hpp \
+  jni/../../depends/include/CELLStream.hpp
+
+jni/../../depends/include\EasyEpollServer.hpp:
+
+jni/../../depends/include/EasyTcpServer.hpp:
+
+jni/../../depends/include/CELL.hpp:
+
+jni/../../depends/include/MessageHeader.hpp:
+
+jni/../../depends/include/CELLTimestamp.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\chrono:
+
+jni/../../depends/include/CELLTask.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\thread:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\mutex:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\list:
+
+jni/../../depends/include/CELLThread.hpp:
+
+jni/../../depends/include/CELLSemaphore.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\condition_variable:
+
+jni/../../depends/include/CELLLog.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\ctime:
+
+jni/../../depends/include/CELLClient.hpp:
+
+jni/../../depends/include/CELLBuffer.hpp:
+
+jni/../../depends/include/CELLNetWork.hpp:
+
+jni/../../depends/include/CELLServer.hpp:
+
+jni/../../depends/include/INetEvent.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map:
+
+jni/../../depends/include/CELLConfig.hpp:
+
+E:/Microsoft/AndroidNDK64/android-ndk-r16b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\atomic:
+
+jni/../../depends/include/CELLEpollServer.hpp:
+
+jni/../../depends/include/CELLEpoll.hpp:
+
+jni/../../depends/include\EasyEpollClient.hpp:
+
+jni/../../depends/include/EasyTcpClient.hpp:
+
+jni/../../depends/include\CELLMsgStream.hpp:
+
+jni/../../depends/include/CELLStream.hpp:
---
+++ tools/test/test/test.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include "test.pb.h"
+
+using namespace std;
+
+int main()
+{
+	IM::Account account1;
+	account1.set_id(1);
+	account1.set_name("windsun");
+	account1.set_password("123456");
+
+	string serializeToStr;
+	account1.SerializeToString(&serializeToStr);
+	cout << "序列化后的字节：" << serializeToStr << endl;
+	cout << "序列化后的长度：" << serializeToStr.length() << endl;
+
+	IM::Account account2;
+	if (!account2.ParseFromString(serializeToStr))
+	{
+		cerr << "failed to parse student." << endl;
+		return -1;
+	}
+	cout << "反序列化：" << endl;
+	cout << account2.id() << endl;
+	cout << account2.name() << endl;
+	cout << account2.password() << endl;
+
+	google::protobuf::ShutdownProtobufLibrary();
+
+	return 0;
+}
